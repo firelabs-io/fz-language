507dc414ac93bbaaaffadb425792f620461ba15a
firelabs-io <fire.labs.oi@gmail.com>
2025-01-06 15:57:39 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # opcodes: mov, add, sub, jmp, jmc, label, ret, end
 import sys
+import time
 
 class machine:
     def __init__(self, program):
@@ -7,6 +8,7 @@ class machine:
         self.reg = {'ax': 0, 'bx': 0, 'cx': 0, 'dx': 0}    
         self.label = {}
         self.stack = []
+        self.debug = False
     def tokenize(self):
         i = 0
         for line in program:
@@ -29,7 +31,6 @@ class machine:
     def run(self):
         i = self.labelc(0)
         while i < len(self.tokens):
-            print(self.reg, self.label, i, self.stack)
             token = self.tokens[i]
             opcode = token[0]
             if opcode == 'mov':
@@ -64,7 +65,6 @@ class machine:
                 if op1 not in self.label:
                     print("ERROR: jumping to label that don't exist")
                     exit(1)
-                print(i)
                 self.stack.append(i)
                 i = self.label[op1]
             elif opcode == 'jmc':
@@ -74,12 +74,34 @@ class machine:
                     exit(1)
                 elif op2 in self.reg:
                     if self.reg[op2] != 0:
-                        self.stack.append(i)
-                        i = self.label[op1]
+                        if self.reg[op2] < 0:
+                            ...
+                        else:
+                            self.stack.append(i)
+                            i = self.label[op1]
                 elif not op2.isdigit():
                     print("ERROR: jump compare need have number or register")
                 else:
                     if int(op2) != 0:
+                        if op2 < 0:
+                            ...
+                        else:
+                            self.stack.append(i)
+                            i = self.label[op1]
+            elif opcode == 'jnc':
+                op1, op2 = token[1], token[2]
+                if not op1 in self.label:
+                    print("ERROR: jumping to label don't exist")
+                    exit(1)
+                elif op2 in self.reg:
+                    if self.reg[op2] == 0:
+                        self.stack.append(i)
+                        i = self.label[op1]
+                elif not op2.isdigit():
+                    print("ERROR: jump compare need have number or register")
+                    exit(1)
+                else:
+                    if int(op2) == 0:
                         self.stack.append(i)
                         i = self.label[op1]
             elif opcode == 'ret':
@@ -99,16 +121,32 @@ class machine:
                     exit(int(op1))
                 else:
                     exit(0)
-            
+            elif opcode == "out":
+                op1 = token[1]
+                if op1 in self.reg:
+                    print(self.reg[op1])
+                else:
+                    print(op1)
+            elif opcode == "in":
+                op1 = token[1]
+                if op1 in self.reg:
+                    inp = int(input())
+                    self.reg[op1] = inp
+                else:
+                    print("ERROR: must have register")
+            if self.debug:
+                print(self.reg, self.label, self.stack, token)
+                time.sleep(0.5)
             i += 1
 
 program = []
 
+
 with open(sys.argv[1], 'r') as file:
     for line in file:
         program.append(line.strip())
-
-
 m = machine(program)
+if len(sys.argv) == 3:
+    m.debug = True
 m.tokenize()
 m.run()
